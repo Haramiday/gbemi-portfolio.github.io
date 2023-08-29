84068ada1bd43624146de84d2fde1e0f01556ccb
Haramiday <45460677+Haramiday@users.noreply.github.com>
2023-08-29 18:22:35 +0100

    first commit

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
+    });
+
+    // NAVBAR
+    
+
+    $('.slick-slideshow').slick({
+      autoplay: true,
+      infinite: true,
+      arrows: false,
+      fade: true,
+      dots: true,
+    });
+
+    $('.slick-testimonial').slick({
+      arrows: false,
+      dots: true,
+    });
+    
+  })(window.jQuery);
